f5b3bf72492720a2487624aca2e64898c7f13927
Luisa Olivares <luisaol25@gmail.com>
2022-10-13 23:56:26 -0300

    calculadora optimizada full

--- index.js
+++ index.js
@@ -1,133 +1,75 @@
+
+// Variables que voy a usar durante el uso de la calculadora
 var num1
 var num2
 var operacion
 
-function suma(num1, num2){
-    return num1 + num2
-     
-}
+// funciones que se gatillan tras eventos desde html (onclick)
 
-function resta(num1, num2){
-    return num1 - num2
-}
+function casoNumeros(numero){
 
-function division(num1, num2){
-    return num1 / num2
-}
+    condicionCero = false
+    let valorCaja = document.getElementById('cajaNumeros').value
 
-function multiplicacion(num1, num2){
-    return num1 * num2
+    if (numero == '0' && document.getElementById('cajaNumeros').value == ""){
+        condicionCero = true
+    }
+
+    if(!condicionCero){
+    document.getElementById("cajaNumeros").value += numero
+    }
 }
 
 function casoC(){
     document.getElementById("cajaNumeros").value = ""
 }
 
-function caso1(){
-    document.getElementById("cajaNumeros").value += "1"  
-}
+function casoOperaciones(tipoDeOperacion){
+    num1 = parseInt(document.getElementById('cajaNumeros').value)
+    casoC()
 
-function caso2(){
-    document.getElementById("cajaNumeros").value += "2"  
-}
-function caso3(){
-    document.getElementById("cajaNumeros").value += "3"  
-}
-function caso4(){
-    document.getElementById("cajaNumeros").value += "4"  
-}
-function caso5(){
-    document.getElementById("cajaNumeros").value += "5"  
-}
-function caso6(){
-    document.getElementById("cajaNumeros").value += "6"  
-}
-function caso7(){
-    document.getElementById("cajaNumeros").value += "7"  
-}
-function caso8(){
-    document.getElementById("cajaNumeros").value += "8"  
-}
-function caso9(){
-    document.getElementById("cajaNumeros").value += "9"  
-}
+    switch (tipoDeOperacion){
 
-function caso0(){
-    let valor = parseInt(document.getElementById("cajaNumeros").value)  
-    if (valor > 0){
-        document.getElementById("cajaNumeros").value += "0" 
+        case "+":
+            operacion = "suma"
+            break
         
-    }          
+        case "-":
+            operacion = "resta"
+            break
+
+        case "x":
+            operacion = "multi"
+            break
+        
+        case "/":
+            operacion = "division"
+    }
 }
 
-function casoSuma(){
 
-    num1 = parseInt(document.getElementById('cajaNumeros').value)
-    operacion = 'suma'
-    casoC()
-    
-}
-
-function casoResta(){
-    num1 = parseInt(document.getElementById('cajaNumeros').value)
-    operacion = 'resta'
-    casoC()
-}
-
-function casoDivision(){
-    num1 = parseInt(document.getElementById('cajaNumeros').value)
-    operacion = 'division'
-    casoC()
-}
-function casoMulti(){
-    num1 = parseInt(document.getElementById('cajaNumeros').value)
-    operacion = 'multi'
-    casoC()
-}
 function casoIgual(){
 
     num2 = parseInt(document.getElementById('cajaNumeros').value)
     
-    /*
-    if (operacion == 'suma'){
-        document.getElementById('cajaNumeros').value = suma(num1, num2)
-    }
-
-    else if(operacion == 'resta' ){
-        document.getElementById('cajaNumeros').value = resta(num1, num2)
-
-    }
- 
-    else if(operacion== 'division'){
-        document.getElementById('cajaNumeros').value = division(num1, num2)
-    }
-
-    else if(operacion== 'multi'){
-        document.getElementById('cajaNumeros').value = multiplicacion(num1, num2)
-    } */
-    
     switch (operacion){
 
         case "suma":
-            document.getElementById('cajaNumeros').value = suma(num1, num2)
+            document.getElementById('cajaNumeros').value = num1 + num2
             break
         
         case "resta":
-            document.getElementById('cajaNumeros').value = resta(num1, num2)
+            document.getElementById('cajaNumeros').value = num1 - num2
             break
         
         case "division":
-            document.getElementById('cajaNumeros').value = division(num1, num2)
+            document.getElementById('cajaNumeros').value = num1 / num2
             break
         
         case "multi":
-            document.getElementById('cajaNumeros').value = multiplicacion(num1, num2)
+            document.getElementById('cajaNumeros').value = num1 * num2
             break
 
     }
 
 }
-
- 
-
-
